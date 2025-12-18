d9f25d0db734f106ec396c40ce42854c8067e28a
Joel <11576776+JokkeeZ@users.noreply.github.com>
2025-12-18 09:23:47 +0200

    Add Example project and update solution structure
    Introduced a new Example project demonstrating usage of WebOSClient, including a sample program to connect and send a toast notification to a webOS TV. Updated the solution file to include both the main library and the new example project, and removed the old solution file from the WebOS.Net directory.

---
+++ Example/Program.cs
@@ -0,0 +1,23 @@
+﻿using WebOS.Net;
+
+using var client = new WebOSClient(
+	// WebOS TV IPv4 address
+	ipAddress: "192.168.0.11",
+
+	// Port 3001 for wss.
+	port: 3001,
+
+	// Provide a client-key or leave empty to obtain a client-key.
+	clientKey: string.Empty
+);
+
+// The TV will display a prompt if a client-key is not provided.
+await client.ConnectAsync();
+
+if (client.IsPaired)
+{
+	Console.WriteLine($"Client Key: {client.ClientKey}");
+
+	// Send Toast notification to webOS TV
+	await client.Notifications.CreateToastAsync("Hello! :)");
+}
